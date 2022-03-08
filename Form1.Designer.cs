e4e4cd699e324176778f5c2d13551b0c404e5c31
ArtemVolkov2103 <82831826+ArtemVolkov2103@users.noreply.github.com>
2022-03-08 12:46:49 +0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace Praktika_WinForms_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(63, 81);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(205, 198);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.InitialImage = null;
+            this.pictureBox2.Location = new System.Drawing.Point(520, 91);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(208, 198);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Файл";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(126, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(360, 31);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(562, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 34);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Открыть";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(63, 285);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(665, 153);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Выберите фильтр";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(22, 99);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(186, 29);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "Удвоение яркости";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(22, 46);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(356, 29);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Зануление всех каналов, кроме синего";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Praktika_WinForms_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Обертка над Bitmap для быстрого чтения и изменения пикселов.
+        /// Также, класс контролирует выход за пределы изображения: при чтении за границей изображения - возвращает DefaultColor, при записи за границей изображения - игнорирует присвоение.
+        /// </summary>
+        public class ImageWrapper : IDisposable, IEnumerable<Point>
+        {
+            /// <summary>
+            /// Ширина изображения
+            /// </summary>
+            public int Width { get; private set; }
+            /// <summary>
+            /// Высота изображения
+            /// </summary>
+            public int Height { get; private set; }
+            /// <summary>
+            /// Цвет по-умолачнию (используется при выходе координат за пределы изображения)
+            /// </summary>
+            public Color DefaultColor { get; set; }
+
+            private byte[] data;//буфер исходного изображения
+            private byte[] outData;//выходной буфер
+            private int stride;
+            private BitmapData bmpData;
+            private Bitmap bmp;
+
+            /// <summary>
+            /// Создание обертки поверх bitmap.
+            /// </summary>
+            /// <param name="copySourceToOutput">Копирует исходное изображение в выходной буфер</param>
+            public ImageWrapper(Bitmap bmp, bool copySourceToOutput = false)
+            {
+                Width = bmp.Width;
+                Height = bmp.Height;
+                this.bmp = bmp;
+
+                bmpData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
+                stride = bmpData.Stride;
+
+                data = new byte[stride * Height];
+                System.Runtime.InteropServices.Marshal.Copy(bmpData.Scan0, data, 0, data.Length);
+
+                outData = copySourceToOutput ? (byte[])data.Clone() : new byte[stride * Height];
+            }
+
+            /// <summary>
+            /// Возвращает пиксел из исходнго изображения.
+            /// Либо заносит пиксел в выходной буфер.
+            /// </summary>
+            public Color this[int x, int y]
+            {
+                get
+                {
+                    var i = GetIndex(x, y);
+                    return i < 0 ? DefaultColor : Color.FromArgb(data[i + 3], data[i + 2], data[i + 1], data[i]);
+                }
+
+                set
+                {
+                    var i = GetIndex(x, y);
+                    if (i >= 0)
+                    {
+                        outData[i] = value.B;
+                        outData[i + 1] = value.G;
+                        outData[i + 2] = value.R;
+                        outData[i + 3] = value.A;
+                    };
+                }
+            }
+
+            /// <summary>
+            /// Возвращает пиксел из исходнго изображения.
+            /// Либо заносит пиксел в выходной буфер.
+            /// </summary>
+            public Color this[Point p]
+            {
+                get { return this[p.X, p.Y]; }
+                set { this[p.X, p.Y] = value; }
+            }
+
+            /// <summary>
+            /// Заносит в выходной буфер значение цвета, заданные в double.
+            /// Допускает выход double за пределы 0-255.
+            /// </summary>
+            public void SetPixel(Point p, double r, double g, double b)
+            {
+                if (r < 0) r = 0;
+                if (r >= 256) r = 255;
+                if (g < 0) g = 0;
+                if (g >= 256) g = 255;
+                if (b < 0) b = 0;
+                if (b >= 256) b = 255;
+
+                this[p.X, p.Y] = Color.FromArgb((int)r, (int)g, (int)b);
+            }
+
+            int GetIndex(int x, int y)
+            {
+                return (x < 0 || x >= Width || y < 0 || y >= Height) ? -1 : x * 4 + y * stride;
+            }
+
+            /// <summary>
+            /// Заносит в bitmap выходной буфер и снимает лок.
+            /// Этот метод обязателен к исполнению (либо явно, лмбо через using)
+            /// </summary>
+            public void Dispose()
+            {
+                System.Runtime.InteropServices.Marshal.Copy(outData, 0, bmpData.Scan0, outData.Length);
+                bmp.UnlockBits(bmpData);
+            }
+
+            /// <summary>
+            /// Перечисление всех точек изображения
+            /// </summary>
+            public IEnumerator<Point> GetEnumerator()
+            {
+                for (int y = 0; y < Height; y++)
+                    for (int x = 0; x < Width; x++)
+                        yield return new Point(x, y);
+            }
+
+            System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
+
+            
+        }
+
+        string filename;
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            filename = textBox1.Text;
+            if (System.IO.File.Exists(filename))
+            {
+                
+                pictureBox1.Image = Image.FromFile(filename);
+                textBox1.Text = filename;
+                radioButton1_CheckedChanged(sender, e);
+            }
+            else
+            {
+                MessageBox.Show("Не удалось открыть изображение");
+            }
+            
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            Bitmap image = (Bitmap)Image.FromFile(filename); 
+
+            using (var wr = new ImageWrapper(image))
+                foreach (var p in wr)
+                    wr.SetPixel(p, 0, 0, wr[p].B);
+            pictureBox2.Image = image;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+           Bitmap image = (Bitmap)Image.FromFile(filename);
+
+            using (var wr = new ImageWrapper(image))
+                foreach (var p in wr)
+                    wr.SetPixel(p, wr[p].R*2, wr[p].G*2, wr[p].B*2);
+            pictureBox2.Image = image;
+        }
+
+        
+    }
+}
